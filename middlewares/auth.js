184c5c1ac35206ad98c0d4aee666eb2638cb5f50
Deo Raj <d.jrajsingh81@gmail.com>
2024-04-20 19:22:28 +0530

    Authentication Middleware Added

---
+++ middlewares/auth.js
@@ -0,0 +1,12 @@
+import JWT from "jsonwebtoken";
+import { errorHandler } from "../utils/errorHandler.js";
+import { User } from "../models/User.js";
+export const isAuthenticated = async (req, res, next) => {
+  const { token } = req.cookies;
+  if (!token) {
+    return next(errorHandler("User must be logged In!", 401));
+  }
+  const decode = await JWT.verify(token, process.env.JWT_SECRET);
+  req.user = await User.findById(decode._id);
+  next();
+};
